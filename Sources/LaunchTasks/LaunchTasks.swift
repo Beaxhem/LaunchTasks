9ffd404e9dcd1f6e16999fa24a2b3bb0e4ae1476
Illia Senchukov <senchukov02@gmail.com>
2023-11-13 00:12:02 +0200

    feat: initial commit

--- Sources/LaunchTasks/LaunchTasks.swift
+++ Sources/LaunchTasks/LaunchTasks.swift
@@ -1,2 +1,74 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public protocol LaunchTask: AnyObject {
+    var next: LaunchTask? { get set }
+
+    func handle()
+    func finish()
+}
+
+extension LaunchTask {
+
+    @discardableResult
+    public func then(_ task: LaunchTask) -> LaunchTask {
+        self.next = task
+        return task
+    }
+
+    public func finish() {
+        self.next?.handle()
+    }
+
+}
+
+public class AsyncLaunchTask: LaunchTask {
+
+    public typealias DismissAction = () -> Void
+
+    public var next: LaunchTask?
+
+    let task: (@escaping DismissAction) -> Void
+
+    public init(task: @escaping (@escaping DismissAction) -> Void) {
+        self.task = task
+    }
+
+    public func handle() {
+        task(finish)
+    }
+
+}
+
+public class UserDefaultsObservingLaunchTask<T>: AsyncLaunchTask {
+
+    let userDefaults: UserDefaults
+    let key: String
+    let filter: (T?) -> Bool
+    let update: () -> T?
+
+    public init(
+        userDefaults: UserDefaults = .standard,
+        key: String,
+        filter: @escaping (T?) -> Bool,
+        task: @escaping (@escaping DismissAction) -> Void,
+        update: @escaping @autoclosure () -> T?
+    ) {
+        self.userDefaults = userDefaults
+        self.key = key
+        self.filter = filter
+        self.update = update
+        super.init(task: task)
+    }
+
+    public override func handle() {
+        let value = userDefaults.value(forKey: key) as? T
+        guard filter(value) else {
+            return finish()
+        }
+        super.handle()
+
+        let newSetting = update()
+        userDefaults.setValue(newSetting, forKey: key)
+    }
+
+}
